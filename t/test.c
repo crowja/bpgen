931fbfaec76d2896216c18021cc9fdeca26d1a98
jac <crowja@gmail.com>
2020-02-17 20:48:10 -0600

    Using tinyhelp.h.

--- t/test.c
+++ t/test.c
@@ -5,93 +5,39 @@
 #include <string.h>
 #include "bpgen.h"
 #include "t/tinytest.h"
-
-#ifdef  COLOR_CODE
-#undef  COLOR_CODE
-#endif
-#define COLOR_CODE       0x1B
-
-#ifdef  COLOR_RED
-#undef  COLOR_RED
-#endif
-#define COLOR_RED        "[1;31m"
-
-#ifdef  COLOR_GREEN
-#undef  COLOR_GREEN
-#endif
-#define COLOR_GREEN      "[1;32m"
-
-#ifdef  COLOR_YELLOW
-#undef  COLOR_YELLOW
-#endif
-#define COLOR_YELLOW     "[1;33m"
-
-#ifdef  COLOR_RESET
-#undef  COLOR_RESET
-#endif
-#define COLOR_RESET      "[0m"
-
-
-static void
-printf_test_name(char *name, char *info)
-{
-   printf("%c%s%s%c%s", COLOR_CODE, COLOR_YELLOW, name, COLOR_CODE, COLOR_RESET);
-
-   if (NULL != info)
-      printf(" [%s]\n", info);
-   else
-      printf("\n");
-}
-
-
-static int
-two_doubles_equal(double x, double y)
-{
-   double      t = fabs(x) + fabs(y);
-   return fabs(x - y) < 4 * DBL_EPSILON * t ? 1 : 0;
-}
-
+#include "t/tinyhelp.h"
 
 static void
 test_constr(void)
 {
    struct bpgen *z;
 
-   printf_test_name("test_constr", "bpgen_new, bpgen_free");
-
+   fprintf_test_info(stdout, "test_constr", "bpgen_new, bpgen_free");
    z = bpgen_new(5);
    ASSERT("Constructor test", z);
    bpgen_free(&z);
    ASSERT_EQUALS(NULL, z);
-
 }
 
-#if 0                                            /* 14 yy */
+#if 0                                            /* 12 yy */
 static void
 test_stub(void)
 {
    struct bpgen *z;
-   double      x = 1.23;                    /* TODO */
 
-   printf_test_name("test_stub", NULL);
-
+   fprintf_test_info(stdout, "test_stub", NULL);
    z = bpgen_new();
    ASSERT("Constructor test, pt 1", z);
-   ASSERT("Here's a test ...", two_doubles_equal(x, 1.23));
    ASSERT_EQUALS(10, 10);
-
    bpgen_free(z);
    ASSERT_EQUALS(NULL, z);
 }
 #endif
 
-
 int
 main(void)
 {
    printf("%s\n", bpgen_version());
-
    RUN(test_constr);
-
    return TEST_REPORT();
 }
